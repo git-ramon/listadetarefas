cf63027f0fd37231406504e1169ebf38d154845b
ramon <ramonrodriguesnw@gmail.com>
2022-08-31 00:06:01 -0300

    função saveTodo

--- js/scripts.js
+++ js/scripts.js
@@ -8,3 +8,10 @@ const cancelEditBtn = document.querySelector("#cancel-edit-btn");
 const searchInput = document.querySelector("#search-input");
 const eraseBtn = document.querySelector("#erase-button");
 const filterBtn = document.querySelector("#filter-select");
+
+let oldInputValue;
+
+// Funções
+const saveTodo = (text, done = 0, save = 1) => {
+  const todo = document.createElement("div");
+  todo.classList.add("todo");
