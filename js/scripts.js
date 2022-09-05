383ce49825232773d2c4589de6e822818278a1e6
ramon <ramonrodriguesnw@gmail.com>
2022-09-04 22:27:11 -0300

    funçao edit form

--- js/scripts.js
+++ js/scripts.js
@@ -163,3 +163,15 @@ cancelEditBtn.addEventListener("click", (e) => {
   e.preventDefault();
   toggleForms();
 });
+
+editForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const editInputValue = editInput.value;
+
+  if (editInputValue) {
+    updateTodo(editInputValue);
+  }
+
+  toggleForms();
+});
